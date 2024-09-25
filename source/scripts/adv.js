3c3597ffb115dadaa98ef5724e06b623c9300a3d
CrashStatic <thecrashstatic@gmail.com>
2024-09-25 15:31:16 +0300

    Поправляет поведение блока advantages на десктопе

--- source/scripts/adv.js
+++ source/scripts/adv.js
@@ -1,6 +1,9 @@
 const advList = document.querySelector('.advantages__list');
 const fragment = document.createDocumentFragment();
 const itemMail = document.querySelector('.advantages__item--mail');
+const radius = 135; // Радиус круга
+const centerX = 105; // Центр по оси X
+const centerY = 165; // Центр по оси Y
 
 const renderItem = () => {
   if (window.innerWidth > 991) {
@@ -13,6 +16,17 @@ const renderItem = () => {
     fragment.append(newItemSetting);
 
     advList.appendChild(fragment);
+
+    const items = advList.querySelectorAll('.advantages__item');
+
+    items.forEach((item, index) => {
+      const angle = (index / items.length) * (2 * Math.PI); // Вычисляем угол
+      const x = centerX + radius * Math.cos(angle) - item.offsetWidth / 2; // Вычисляем X координату
+      const y = centerY + radius * Math.sin(angle) - item.offsetHeight / 2; // Вычисляем Y координату
+
+      item.style.left = `${x}px`; // Устанавливаем позицию X
+      item.style.top = `${y}px`; // Устанавливаем позицию Y
+    });
   }
 };
 
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,22 +5,6 @@ import { renderItem } from './adv.js';
 import './stock-counter.js';
 import './bubble.js';
 
-const items = document.querySelectorAll('.advantages__item');
-const radius = 135; // Радиус круга
-const centerX = 105; // Центр по оси X
-const centerY = 165; // Центр по оси Y
-
 initMenu();
 initOffersSlider();
 renderItem();
-
-if (window.innerWidth > 991) {
-  items.forEach((item, index) => {
-    const angle = (index / items.length) * (2 * Math.PI); // Вычисляем угол
-    const x = centerX + radius * Math.cos(angle) - item.offsetWidth / 2; // Вычисляем X координату
-    const y = centerY + radius * Math.sin(angle) - item.offsetHeight / 2; // Вычисляем Y координату
-
-    item.style.left = `${x}px`; // Устанавливаем позицию X
-    item.style.top = `${y}px`; // Устанавливаем позицию Y
-  });
-}
